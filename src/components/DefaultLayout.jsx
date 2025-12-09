103ddf794957d85a6f55b1322b1bb94592a500c1
martinagiammillari <martigiammi@gmail.com>
2025-12-09 15:00:52 +0100

    feat: home, about, products in progress

---
+++ src/components/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout(){
+return(
+<>
+<header>HEADER</header>
+<main>
+    <Outlet/>
+</main>
+<footer>FOOTER</footer>
+
+
+</>
+)
+}
---
+++ src/components/Products.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <>
+      <h1>Ciao sono PRODUCTS</h1>
+    </>
+  );
+}
